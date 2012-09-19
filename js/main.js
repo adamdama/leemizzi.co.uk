46ec12a868ec7654f383b92713495962c8e991b2
Adam Cox <adamdama@hotmail.com>
2012-09-19 22:46:28 +0100

    Rail width and height is now set

--- js/main.js
+++ js/main.js
@@ -5,10 +5,11 @@
 $(document).ready(function()
 {
 	var imageFolder = 'images/portfolio';
-	var images = ['beached-book-cover-detail.jpg', 'behave-yourself-design-branding.jpg', 'behold-blue-college-piece.jpg', 'belated-god-logo.jpg', 'dbu-online-brochure-spread.jpg', 'fierce-pretty-things-college-project.jpg', 'flight-poster-design.jpg', 'giraffe-magazine-spread.jpg', 'horse-college-project.jpg', 'ioti-print-brochure.jpg', 'i-studentglobal-website-design.jpg', 'life-website-web-design-branding.jpg', 'me.jpg', 'sussexpeopledevelopment-branding-and-website.jpg', 'w-typography.jpg'];
+	var images = ['beached-book-cover-detail.jpg', 'behave-yourself-design-branding.jpg', 'behold-blue-college-piece.jpg', 'belated-god-logo.jpg', 'dbu-online-brochure-spread.jpg', 'fierce-pretty-things-college-project.jpg', 'flight-poster-design.jpg', 'giraffe-magazine-spread.jpg', 'horse-college-project.jpg', 'ioti-print-brochure.jpg', 'i-studentglobal-website-design.jpg', 'life-website-web-design-branding.jpg', 'sussexpeopledevelopment-branding-and-website.jpg', 'w-typography.jpg'];
 
 	var rail = $('#rail');
 	var railWidth = 0;
+	var railHeight = 0;
 
 	$(images).each(function()
 	{
@@ -16,9 +17,16 @@ $(document).ready(function()
 		.appendTo(rail)
 		.one("load", function()
 		{
-			railWidth += $(this).width();
+			var $this = $(this);
+			
+			$this.data('xPos', railWidth);
+			
+			railWidth += $this.outerWidth(true);
 			rail.width(railWidth);
-
+			
+			if($this.height() > railHeight)
+				rail.height(railHeight = $this.height());
+			
 			return false;
 		})
 		.each(function()
