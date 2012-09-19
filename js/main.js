a40776c0f67df5e70e39d8e02930f737fd8365b9
Adam Cox <adamdama@hotmail.com>
2012-09-19 21:37:59 +0100

    images now added to gallery

--- js/main.js
+++ js/main.js
@@ -2,5 +2,11 @@
  * @author: Adam Cox
  * @email: adamdama@hotmail.com
  */
+var imageFolder = 'images';
+var images = ['beached-book-cover-detail.jpg', 'behave-yourself-design-branding.jpg', 'behold-blue-college-piece.jpg', 'belated-god-logo.jpg', 'dbu-online-brochure-spread.jpg', 'fierce-pretty-things-college-project.jpg', 'flight-poster-design.jpg', 'giraffe-magazine-spread.jpg', 'horse-college-project.jpg', 'ioti-print-brochure.jpg', 'i-studentglobal-website-design.jpg', 'life-website-web-design-branding.jpg', 'me.jpg', 'sussexpeopledevelopment-branding-and-website.jpg', 'w-typography.jpg'];
 
-var images = ['images/'];
+$(images).each(function()
+{
+	$('<img />').attr('src', imageFolder + '/' +this.toString()).appendTo('#gallery');
+});
+
