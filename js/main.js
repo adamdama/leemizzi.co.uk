b1999af9061791b01aa4605ae404197269edab35
Adam Cox <adamdama@hotmail.com>
2012-09-19 22:16:17 +0100

    Added image load to set width of rail

--- js/main.js
+++ js/main.js
@@ -2,11 +2,30 @@
  * @author: Adam Cox
  * @email: adamdama@hotmail.com
  */
-var imageFolder = 'images';
-var images = ['beached-book-cover-detail.jpg', 'behave-yourself-design-branding.jpg', 'behold-blue-college-piece.jpg', 'belated-god-logo.jpg', 'dbu-online-brochure-spread.jpg', 'fierce-pretty-things-college-project.jpg', 'flight-poster-design.jpg', 'giraffe-magazine-spread.jpg', 'horse-college-project.jpg', 'ioti-print-brochure.jpg', 'i-studentglobal-website-design.jpg', 'life-website-web-design-branding.jpg', 'me.jpg', 'sussexpeopledevelopment-branding-and-website.jpg', 'w-typography.jpg'];
-
-$(images).each(function()
+$(document).ready(function()
 {
-	$('<img />').attr('src', imageFolder + '/' +this.toString()).appendTo('#gallery');
+	var imageFolder = 'images/portfolio';
+	var images = ['beached-book-cover-detail.jpg', 'behave-yourself-design-branding.jpg', 'behold-blue-college-piece.jpg', 'belated-god-logo.jpg', 'dbu-online-brochure-spread.jpg', 'fierce-pretty-things-college-project.jpg', 'flight-poster-design.jpg', 'giraffe-magazine-spread.jpg', 'horse-college-project.jpg', 'ioti-print-brochure.jpg', 'i-studentglobal-website-design.jpg', 'life-website-web-design-branding.jpg', 'me.jpg', 'sussexpeopledevelopment-branding-and-website.jpg', 'w-typography.jpg'];
+
+	var rail = $('#rail');
+	var railWidth = 0;
+
+	$(images).each(function()
+	{
+		$('<img />').attr('src', imageFolder + '/' + this.toString())
+		.appendTo(rail)
+		.one("load", function()
+		{
+			railWidth += $(this).width();
+			rail.width(railWidth);
+
+			return false;
+		})
+		.each(function()
+		{
+			if (this.complete || (jQuery.browser.msie && parseInt(jQuery.browser.version) == 6))
+				jQuery(this).trigger("load");
+		});
+	});
 });
 
