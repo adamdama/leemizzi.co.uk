b597440f70fd9707b2677becde99e7149196b65d
Adam Cox <adamdama@hotmail.com>
2012-09-21 00:19:34 +0100

    Changed equalities to strict

--- js/main.js
+++ js/main.js
@@ -6,7 +6,9 @@ $(document).ready(function()
 {
 	var imageFolder = 'images/portfolio';
 	var images = ['beached-book-cover-detail.jpg', 'behave-yourself-design-branding.jpg', 'behold-blue-college-piece.jpg', 'belated-god-logo.jpg', 'dbu-online-brochure-spread.jpg', 'fierce-pretty-things-college-project.jpg', 'flight-poster-design.jpg', 'giraffe-magazine-spread.jpg', 'horse-college-project.jpg', 'ioti-print-brochure.jpg', 'i-studentglobal-website-design.jpg', 'life-website-web-design-branding.jpg', 'sussexpeopledevelopment-branding-and-website.jpg', 'w-typography.jpg'];
-
+	
+	var thumbnails = [];
+	
 	var rail = $('#rail');
 	var railWidth = 0;
 	var railHeight = 0;
@@ -25,22 +27,24 @@ $(document).ready(function()
 				rail.width(railWidth);
 
 				if ($this.height() > railHeight)
-					rail.height( railHeight = $this.height());
-				
-				if($this.index() == rail.children().length - 1)
+					rail.height(railHeight = $this.height());
+
+				if ($this.index() === rail.children().length - 1)
 					centerRail();
-					
-				if($this.index() === 0)
+
+				if ($this.index() === 0)
 					$this.show();
 					
+				
+
 				return false;
 			}).each(function()
 			{
-				if (this.complete || (jQuery.browser.msie && parseInt(jQuery.browser.version) == 6))
+				if (this.complete || (jQuery.browser.msie && parseInt(jQuery.browser.version) === 6))
 					jQuery(this).trigger("load");
 			});
 		});
-		
+
 		$(window).resize(centerRail);
 	};
 
@@ -52,9 +56,9 @@ $(document).ready(function()
 		}).first();
 
 		var xPos = ($(window).width() - image.width()) / 2;
-		
+
 		rail.css('left', xPos + 'px');
 	};
 
 	init();
-});
+}); 
